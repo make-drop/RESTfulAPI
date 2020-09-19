3a3bda4fe3fc1af4a27fa663feea5f5c8f4f4c11
user1 <m.gatti35@campus.unimib.it>
2020-09-19 16:43:11 +0200

    first commit

---
+++ src/main/java/com/api/restdefinitive/demo/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.api.restdefinitive.demo.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/test/java/com/api/restdefinitive/demo/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.restdefinitive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
